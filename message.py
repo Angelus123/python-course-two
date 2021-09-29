d17926c41856186e141d62ebf5adafdcd2ad291c
Angelus123 <izerefaifelix@gmail.com>
2021-09-29 09:34:18 -0400

    Escape sequence

---
+++ message.py
@@ -0,0 +1,14 @@
+# message = 'pyton"Programming'
+# print(message)
+
+# Escape sequence
+# for comments
+# \" to desplay " in a string
+# \' to display ' in a string
+# \\' to display back slash
+# \n' for new line
+
+
+
+message = "pyton\"Programming"
+print(message)
